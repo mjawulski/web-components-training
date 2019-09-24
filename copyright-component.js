8bf342f98adbb8078bf54300aa71df80aa652f28
Michał Jawulski <mjawulski@gmail.com>
2019-09-24 11:48:08 +0200

    added content

--- copyright-component.js
+++ copyright-component.js
@@ -1,6 +1,16 @@
+let template = document.createElement('template');
+template.innerHTML = `
+&copy; My awesome company 2019
+`;
+
 class CopyrightNotice extends HTMLElement {
   constructor() {
     super();
+
+    let templateClone = template.content.cloneNode(true);
+    this.attachShadow({ mode: 'open' });
+
+    this.shadowRoot.appendChild(templateClone);
   }
 }
 
